1534676204d910c96ef02996a3ef1e25f7617bf2
qaplanet <qaplanet@gmail.com>
2017-12-10 12:12:38 +0530

    First project

---
+++ CRM_Automation/src/main/java/org/qaplanet/in/LIB.java
@@ -0,0 +1,23 @@
+package org.qaplanet.in;
+
+public class LIB {
+	public void method1()
+	{
+		System.out.println("Method1");
+	}
+	
+	public void method2()
+	{
+		System.out.println("Method2");
+	}
+	
+	public void method3()
+	{
+		System.out.println("Method3");
+	}
+	
+	public void method4()
+	{
+		System.out.println("Method4");
+	}
+}
